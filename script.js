4f5506e75afc0a7856d175a5365bdfb828b9a034
leutzendorff <138122380+leutzendorff@users.noreply.github.com>
2023-06-29 14:36:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-document.getElementById('searchForm').addEventListener('submit', function(e) {
-    e.preventDefault(); // verhindert das automatische Absenden des Formulars
+function onGet(version) {
+    // Stellen Sie sicher, dass das Protokoll, der Host und der Port korrekt sind.
+    const url = "http://localhost:8000/api/" + version + "/messages";
+    var headers = {
+        // Die 'Origin'-Kopfzeile zeigt an, von welcher Domain die Anfrage stammt.
+        'Origin': 'http://localhost:8080'
+    }
 
-    let lyrics = document.getElementById('lyrics').value;
-    let apiKey = 'c386b28656b34620b5d2c7adc725bd7a';
-
-    // Wir fügen 'https://thingproxy.freeboard.io/fetch/' vor die URL, um das CORS-Problem zu umgehen.
-    fetch('https://cors.bridged.cc/https://api.musixmatch.com/ws/1.1/track.search?q_lyrics=' + lyrics + '&apikey=' + apiKey)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            let output = '<h2>Suchergebnisse</h2>';
-
-            // Überprüfen Sie, ob Daten vorhanden sind
-            if(data.message.body.track_list.length === 0) {
-                output += '<p>Keine Übereinstimmungen gefunden</p>';
-            } else {
-                data.message.body.track_list.forEach(function(track) {
-                    output += `
-                        <h3>${track.track.track_name} by ${track.track.artist_name}</h3>
-                        <p>${track.track.album_name}</p>
-                        <p><a href="${track.track.track_share_url}">Song Link</a></p>
-                    `;
-                });
-            }
-
-            document.getElementById('results').innerHTML = output;
-        })
-        .catch(err => console.log(err));
-});
+    fetch(url, {
+        method: "GET",
+        // Der Modus 'cors' gibt an, dass der Browser eine CORS-Anfrage senden soll.
+        mode: 'cors',
+        headers: headers
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+        document.getElementById('messages').value = data.messages;
+    })
+    .catch(function(error) {
+        document.getElementById('messages').value = error.message;
+    });
+}
