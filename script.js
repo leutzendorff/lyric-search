b3f07f4391472f16b28586009e83f1658a168519
leutzendorff <138122380+leutzendorff@users.noreply.github.com>
2023-06-29 15:20:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.getElementById('form1');
+document.getElementById('lyricForm').addEventListener('submit', function(e) {
+    e.preventDefault();
 
-    if (form) {
-        form.addEventListener('submit', function(e) {
-            e.preventDefault();
+    let lyrics = document.getElementById('lyrics').value;
 
-            // get the value from the input field
-            const lyrics = document.getElementById('lyrics').value;
-
-            // define the url for the api call
-            const url = `https://api.allorigins.win/raw?url=${encodeURIComponent(`https://api.musixmatch.com/ws/1.1/track.search?q_lyrics=${lyrics}&apikey=c386b28656b34620b5d2c7adc725bd7a`)}`;
-
-            fetch(url)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error("HTTP error " + response.status);
-                }
-                return response.json();
-            })
-            .then(json => {
-                console.log(json);
-            })
-            .catch(function() {
-                console.log("An error occurred while fetching the API data.");
-            });
+    let proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+        targetUrl = `https://api.musixmatch.com/ws/1.1/track.search?q_lyrics=${lyrics}&apikey=c386b28656b34620b5d2c7adc725bd7a`
+    fetch(proxyUrl + targetUrl)
+        .then(blob => blob.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(e => {
+            console.log(e);
+            return e;
         });
-    } else {
-        console.log('Form element not found.');
-    }
 });
