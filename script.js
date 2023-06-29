e6261ce4f985327de6519c2aaee1f16e6d5d0d69
leutzendorff <138122380+leutzendorff@users.noreply.github.com>
2023-06-29 15:36:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.querySelector('form');
-    const input = document.querySelector('input');
-    const results = document.querySelector('.results');
+document.addEventListener('DOMContentLoaded', (event) => {
+    function onGet(version) {
+        const url = "https://api.lyrics.ovh/v1/Coldplay/Adventure of a Lifetime";
+        var headers = {}
 
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const api = `${proxy}https://api.deezer.com/search/`;
-
-    form.addEventListener('submit', e => {
-        e.preventDefault();
-        const userSearch = input.value;
-        fetch(`${api}?q=${userSearch}`)
-        .then(response => response.json())
-        .then(data => {
-            let output = '';
-            data.data.forEach(item => {
-                output += `
-                    <li>
-                        <h3>${item.artist.name}</h3>
-                        <p>${item.title}</p>
-                    </li>
-                `;
-            });
-            results.innerHTML = output;
+        fetch(url, {
+            method : "GET",
+            mode: 'cors',
+            headers: headers
         })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.error)
+            }
+            return response.json();
+        })
+        .then(data => {
+            document.getElementById('messages').innerText = data.lyrics;
+        })
+        .catch(function(error) {
+            document.getElementById('messages').innerText = error;
+        });
+    }
+
+    document.getElementById('form1').addEventListener('submit', function(e) {
+        e.preventDefault(); // Verhindert das Neuladen der Seite
+        onGet('v1');
     });
 });
